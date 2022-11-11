806f876a0f73e12540588f6f41396b802ca258fb
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-11 16:00:00 +0900

    Create sensor.py

---
+++ code/sensor.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+import time
+
+TrigPin = 16
+EchoPin = 20
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(TrigPin, GPIO.OUT)
+GPIO.setup(EchoPin, GPIO.IN)
+
+def distance():   
+    GPIO.output(TrigPin, True)
+    time.sleep(0.00001)
+    GPIO.output(TrigPin, False)
+    
+    while GPIO.input(EchoPin) == 0:
+        start_time = time.time()
+        
+    while GPIO.input(EchoPin) == 1 :
+        end_time = time.time()
+        
+    duration = end_time - start_time
+    
+    distanceCm = duration* 17000
+    distanceCm = round(distanceCm, 2)
+    return distanceCm
+
+try :
+    while True:
+        distanceCm = distance()
+        
+        print("cm : ", distanceCm)
+        time.sleep(0.5)
+        
+except KeyboardInterrupt :
+    pass
+
+GPIO.cleanup()
