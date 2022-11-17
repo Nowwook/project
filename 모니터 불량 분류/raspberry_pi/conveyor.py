fd77f995d25b5a1af855254280564835f4144630
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-17 21:55:00 +0900

    Create conveyor.py

---
+++ 모니터 불량 분류/raspberry_pi/conveyor.py
@@ -0,0 +1,62 @@
+import RPi.GPIO as GPIO
+import time
+from collections import deque
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+AIN1=5
+BIN1=7
+AIN2=11
+BIN2=13
+a=3
+b=15
+
+sig=deque([1,0,0,0])
+
+GPIO.setup(AIN1,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(BIN1,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(AIN2,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(BIN2,GPIO.OUT,initial=GPIO.LOW)
+
+GPIO.setup(a,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(b,GPIO.OUT,initial=GPIO.LOW)
+
+def motor(s,r):
+    GPIO.output(a,1)
+    GPIO.output(b,1)
+    t = int(s*5/9)
+    for cnt in range(0,t):
+        GPIO.output(AIN1,sig[0])
+        GPIO.output(BIN1,sig[1])
+        GPIO.output(AIN2,sig[2])
+        GPIO.output(BIN2,sig[3])
+        time.sleep(0.01)
+        
+        sig.rotate(r)  
+    GPIO.output(a,0)
+    GPIO.output(b,0)
+
+global location
+location = 2
+
+# 첫 위치 2, 정상=2, 버림=1, 수리가능=3,4
+def moving(CLASSIFY_RESULT):
+    Way = -1
+    global location
+    
+    if CLASSIFY_RESULT==4:
+        CLASSIFY_RESULT=3
+        
+    move = CLASSIFY_RESULT - location
+    if move<0:
+        move = -move
+        Way = 1
+
+    if move != 0:
+        motor(45*int(move),Way)
+        
+    location = CLASSIFY_RESULT
+    
+def run():
+    print( "conveyor running" )
