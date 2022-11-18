a76cde4e2a7cfb831bead1ebca8193419e9550a3
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-18 23:45:53 +0900

    Update sensor_capture.py

--- 모니터 불량 분류/raspberry_pi/sensor_capture.py
+++ 모니터 불량 분류/raspberry_pi/sensor_capture.py
@@ -43,12 +43,22 @@ def detected():
                 camera.stop_preview()
                 
                 img = cv2.imread('./img/capture.png') 
-                det = cv2.QRCodeDetector()  
-                info, box_coordinates, _ = det.detectAndDecode(img)
-                if box_coordinates is None: 
-                    print('No Code')
-                else:
-                    print(info) 
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                decoded = pyzbar.decode(gray)
+
+                for d in decoded: 
+                    x, y, w, h = d.rect
+
+                    barcode_data = d.data.decode("utf-8")
+                    barcode_type = d.type
+
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+                    text = '%s (%s)' % (barcode_data, barcode_type)
+                    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
+                    print(barcode_data)
+                return barcode_data
+                    
             print("cm : ", distanceCm)
             time.sleep(0.5)
             
