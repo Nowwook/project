fbb303d66ae590dc8cb4fcae23bd4a342fbf6135
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-19 17:38:10 +0900

    Update camera.py

--- 모니터 불량 분류/raspberry_pi/camera.py
+++ 모니터 불량 분류/raspberry_pi/camera.py
@@ -1,109 +1,102 @@
 import cv2
 import numpy as np
+import time
+from picamera import PiCamera
+from time import sleep
 
-def capture(a):
-    image = cv2.imread("./Image/"+a+".png")
-    image_gray = cv2.imread("./Image/"+a+".png", cv2.IMREAD_GRAYSCALE) 
+
+def capture():
+    PiCamera.capture('a.png')
+    img = cv2.imread('a.png')
+    return img
+    
+def qr(img):
+    decoded = pyzbar.decode(img)
+
+    for d in decoded: 
+        x, y, w, h = d.rect
+
+        barcode_data = d.data.decode("utf-8")
+        return barcode_data
+
+def classify(img):
+    image_gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE) 
 
     blur = cv2.GaussianBlur(image_gray, ksize=(5,5), sigmaX=0)
-edged = cv2.Canny(blur, 50, 250)
-cv2.imshow('Edged', edged)
-cv2.waitKey(0)
+    ret, thresh1 = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
-closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+    edged = cv2.Canny(blur, 50, 250)
 
-contours, _ = cv2.findContours(closed.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+    closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
 
-contours_xy = np.array(contours)
-contours_xy.shape
+    contours, _ = cv2.findContours(closed.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-x_min, x_max = 0,0
-value = list()
-for i in range(len(contours_xy)):
-   for j in range(len(contours_xy[i])):
-      value.append(contours_xy[i][j][0][0]) 
-      x_min = min(value)
-      x_max = max(value)
-      
-y_min, y_max = 0,0
-value = list()
-for i in range(len(contours_xy)):
-   for j in range(len(contours_xy[i])):
-      value.append(contours_xy[i][j][0][1]) 
-      y_min = min(value)
-      y_max = max(value)
+    contours_xy = np.array(contours)
+    contours_xy.shape
+    x_min, x_max = 0,0
 
-if (x_max - x_min) <=1000:
-   x_min=30
-   y_min=30
+    value = list()
+    for i in range(len(contours_xy)):
+        for j in range(len(contours_xy[i])):
+            value.append(contours_xy[i][j][0][0]) 
+            x_min = min(value)
+            x_max = max(value)
+            
+    y_min = 0
+    value = list()
 
-img_trim = image[y_min+30:y_min+610, x_min+30:x_min+1210]
-bgr = img_trim[:,:,:3]
+    for i in range(len(contours_xy)):
+        for j in range(len(contours_xy[i])):
+            value.append(contours_xy[i][j][0][1]) 
+            y_min = min(value)
 
-CLASSIFY_RESULT = 4
+    if (x_max - x_min) <=1000:
+        x_min=30
+        y_min=30
 
-cv2.imshow("Image1", bgr)
-cv2.waitKey(0)
+    img_trim = img[y_min+30:y_min+610, x_min+30:x_min+1210]
+    bgr = img_trim[:,:,:3]
 
-color_mean = cv2.mean(bgr)
-print(color_mean)
+    CLASSIFY_RESULT = 4
 
-if round(color_mean[0])==round(color_mean[1])==round(color_mean[2]):
-    if color_mean[0]<100:
-        CLASSIFY_RESULT = 3     # hot
-    else:
-        CLASSIFY_RESULT = 1     # dead
-        out = bgr.copy()
-        bgr = 255 - out
-        
-    blurred = cv2.GaussianBlur(bgr, (5, 5), 0)
-
-    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Image2", gray)
-    cv2.waitKey(0)
-
-    ret, thresh = cv2.threshold(gray, 10, 250, cv2.THRESH_BINARY)
-    thresh = cv2.erode(thresh, None, iterations=2)
-
-    contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    color_mean = cv2.mean(bgr)
+    # print(color_mean)
+    if color_mean[0]<20 and color_mean[1]<20 and color_mean[2]<20:
+        CLASSIFY_RESULT = 3
+    if color_mean[0]>240 and color_mean[1]>240 and color_mean[2]>240:
+        CLASSIFY_RESULT = 1
     
-else:
-    if round(color_mean[0]) > (round(color_mean[1]) and round(color_mean[2])): 
-        mask = cv2.inRange( bgr, (0,0,0), (255,0,0))
-        bgr[mask==255] = (0,0,0)
-    if round(color_mean[1]) > (round(color_mean[0]) and round(color_mean[2])): 
-        mask = cv2.inRange( bgr, (0,0,0), (0,255,0))
-        bgr[mask==255] = (0,0,0)    
-    if round(color_mean[2]) > (round(color_mean[1]) and round(color_mean[0])): 
-        mask = cv2.inRange( bgr, (0,0,0), (0,0,255))
-        bgr[mask==255] = (0,0,0)
+    
+    mask = cv2.inRange( bgr, (0,0,0), (0,255,0))
+    bgr[mask==255] = (0,0,0)
+
+    mask = cv2.inRange( bgr, (0,0,0), (255,0,0))
+    bgr[mask==255] = (0,0,0)
+
+    mask = cv2.inRange( bgr, (0,0,0), (0,0,255))
+    bgr[mask==255] = (0,0,0)
 
     blurred = cv2.GaussianBlur(bgr, (5, 5), 0)
 
     gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Image2", gray)
-    cv2.waitKey(0)
-
     ret, thresh = cv2.threshold(gray, 10, 250, cv2.THRESH_BINARY)
     thresh = cv2.erode(thresh, None, iterations=2)
 
     contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-if len(contours) == 2:
-   contours = contours[0]
-elif len(contours) == 3:
-   contours = contours[1]
+    if len(contours) == 2:
+        contours = contours[0]
+    elif len(contours) == 3:
+        contours = contours[1]
 
-cnt = len(contours)
-color_mean2 = cv2.mean(bgr)
-if color_mean2[0]>250 and color_mean2[1]>250 and color_mean2[2]>250:
-   cnt = cnt-1
+    cnt = len(contours)
+    if color_mean[0]>250 and color_mean[1]>250 and color_mean[2]>250:
+        cnt = cnt-1
 
-print(color_mean2)
-print('cnt:', cnt)
+    # print('cnt:', cnt)
 
-if cnt<=0:
-   CLASSIFY_RESULT=2
-
-return CLASSIFY_RESULT
+    if cnt==0:
+        CLASSIFY_RESULT=2
+        
+    return CLASSIFY_RESULT
