b70d5219f882e273148f3b775966181ff839585c
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-23 14:54:11 +0900

    Update main.py

--- 모니터 불량 분류/raspberry_pi/main.py
+++ 모니터 불량 분류/raspberry_pi/main.py
@@ -1,6 +1,6 @@
 from threading import Thread
 import time
-
+import cv2
 import socket_client
 import camera
 import conveyor
@@ -8,22 +8,45 @@ import sensor
 
 def running():
     try:
+        global now # 벨트 위치
+        now = 2
+        
         while True:
             time.sleep( 1 )
-            recv_data_d = socket_client.recv_d()
-            if recv_data_d == "REQUEST_RESULT":
-                CLASSIFY_RESULT_d = camera.capture(str("stuck1"))
-                socket_client.send(CLASSIFY_RESULT_d)
-                # conveyor.moving(CLASSIFY_RESULT)
-                socket_client.send("ROLLING_END")
-    
+            recv_data_1 = socket_client.recv_d()
+            if recv_data_1 == "START":
+                while 1:
+                    data_1 = sensor.distance_1()
+                    if data_1 <= 10:
+                        time.sleep(3) # 놓는 시간
+                        capture = camera.capture()
+                        qr = camera.qr(capture)
+                        CLASSIFY_RESULT_d = camera.classify(capture)
+                        
+                        # print(qr, CLASSIFY_RESULT_d)
+                        socket_client.send("RESULT,"+qr+","+CLASSIFY_RESULT_d)
+                        conveyor.moving(CLASSIFY_RESULT_d,now)
+                        result_1 = conveyor.run()
+
+                        while 1:
+                            recv_data_2 = socket_client.recv_d()
+                            if recv_data_2=="DB_END":
+                                break
+                                
+                        if result_1 == 0:
+                            socket_client.send("ROLLING_END")
+                        else:
+                            socket_client.send("STUCK")
+                        break
+
     except Exception as e:
         print( type(e) )
 
 
 if __name__ == "__main__":
     print( "main" )
-    socket_client.connect_d('192~~, 8000 )
+    socket_client.connect_d('192.168.0.35', 8000 )
     
     main_th = Thread( target=running )
     main_th.start()
+    # running()
