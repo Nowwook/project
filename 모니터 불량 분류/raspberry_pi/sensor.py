6cfaae4338fcb507d87596d46e6aeefe9ff8b416
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-26 22:10:19 +0900

    Update sensor.py

--- 모니터 불량 분류/raspberry_pi/sensor.py
+++ 모니터 불량 분류/raspberry_pi/sensor.py
@@ -1,57 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
-TrigPin_1 = 16
-EchoPin_1 = 20
-# TrigPin_2 = 16
-# EchoPin_2 = 20
 m = 15
+n = 16
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(TrigPin_1, GPIO.OUT)
-GPIO.setup(EchoPin_1, GPIO.IN)
-# GPIO.setup(TrigPin_2, GPIO.OUT)
-# GPIO.setup(EchoPin_2, GPIO.IN)
-
-def distance_1():   
-    GPIO.output(TrigPin_1, True)
-    time.sleep(0.00001)
-    GPIO.output(TrigPin_1, False)
-    
-    while GPIO.input(EchoPin_1) == 0:
-        start_time = time.time()
-        
-    while GPIO.input(EchoPin_1) == 1 :
-        end_time = time.time()
-        
-    duration = end_time - start_time
-    
-    distanceCm = duration* 17000
-    distanceCm = round(distanceCm, 2)
-    return distanceCm
-
-def distance_2():   
-    GPIO.output(TrigPin_2, True)
-    time.sleep(0.00001)
-    GPIO.output(TrigPin_2, False)
-    
-    while GPIO.input(EchoPin_2) == 0:
-        start_time = time.time()
-        
-    while GPIO.input(EchoPin_2) == 1 :
-        end_time = time.time()
-        
-    duration = end_time - start_time
-    
-    distanceCm = duration* 17000
-    distanceCm = round(distanceCm, 2)
-    return distanceCm
-
-def distance_3():
+def INIT_1():
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(m, GPIO.IN)
+    GPIO.setup(n, GPIO.IN)
+
+def start_pos_detect():
     return GPIO.input(m)
+
+def slide_detect():
+    return GPIO.input(n)
     
 GPIO.cleanup()
