4effa9ca4cd1054cae610a99fd9a53c43fe86264
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-26 22:05:57 +0900

    Update main.py

--- 모니터 불량 분류/raspberry_pi/main.py
+++ 모니터 불량 분류/raspberry_pi/main.py
@@ -6,38 +6,63 @@ import camera
 import conveyor
 import sensor
 
+timer_work = False
+start_time = time.time()
+end_time = time.time()
+
+def timer_start():
+    timer_check = True
+    start_time = time.time()
+
+def timer_check():
+    end_time = time.time()
+    return end_time - start_time
+
+def timer_stop():
+    timer_work = False
+
+def timer_working():
+    return timer_work
+
+
+
 def running():
     try:
-        global now # 벨트 위치
-        now = 2
-        
         while True:
             time.sleep( 1 )
-            recv_data_1 = socket_client.recv_d()
-            if recv_data_1 == "START":
-                while 1:
-                    data_1 = sensor.distance_1()
-                    if data_1 <= 10:
-                        time.sleep(3) # 놓는 시간
-                        capture = camera.capture()
-                        qr = camera.qr(capture)
-                        CLASSIFY_RESULT_d = camera.classify(capture)
-                        
-                        # print(qr, CLASSIFY_RESULT_d)
-                        socket_client.send("RESULT,"+qr+","+CLASSIFY_RESULT_d)
-                        conveyor.moving(CLASSIFY_RESULT_d,now)
-                        result_1 = conveyor.run()
+            recv_data = socket_client.recv_d()
+            start_sensor_state = sensor.start_pos_detect()
+            slide_sensor_state = sensor.slide_detect()
 
-                        while 1:
-                            recv_data_2 = socket_client.recv_d()
-                            if recv_data_2=="DB_END":
-                                break
-                                
-                        if result_1 == 0:
-                            socket_client.send("ROLLING_END")
-                        else:
-                            socket_client.send("STUCK")
-                        break
+            if recv_data == "START":
+                if start_sensor_state == 1:
+                    capture_img = camera.capture()
+                    qr_data = camera.qr(capture_img)
+                    CLASSIFY_RESULT = camera.classify(capture_img)
+                    socket_client.send("RESULT,"+qr_data+","+CLASSIFY_RESULT)
+                    
+                    pos = conveyor.Position.MIDDLE
+                    if pos == 1:
+                        pos = conveyor.Position.TOP
+                    elif pos == 2:
+                        pos = conveyor.Position.MIDDLE
+                    else:
+                        pos = conveyor.Position.BOTTOM
+
+                    now = conveyor.moving(pos)
+                    conveyor.run()
+                    timer_start()
+
+
+            working_time = timer_check()
+            if slide_sensor_state == 1:
+                timer_stop()
+                conveyor.stop()
+                socket_client.send("ROLLING_END")
+            
+            if timer_working() and working_time > 2:
+                timer_stop()
+                socket_client.send("STUCK")
 
     except Exception as e:
         print( type(e) )
@@ -49,4 +74,3 @@ if __name__ == "__main__":
     
     main_th = Thread( target=running )
     main_th.start()
-    # running()
