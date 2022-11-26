7ed215e68ee790b3b9ac20ae14581fd51598583d
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-26 22:07:15 +0900

    Update conveyor.py

--- 모니터 불량 분류/raspberry_pi/conveyor.py
+++ 모니터 불량 분류/raspberry_pi/conveyor.py
@@ -2,23 +2,35 @@ import RPi.GPIO as GPIO
 import time
 from collections import deque
 import sensor
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+from enum import Enum
 
 AIN1=3
 BIN1=7
 AIN2=5
 BIN2=11
-conveyor = 13
+con = 13
 
 sig=deque([1,0,0,0])
 
-GPIO.setup(AIN1,GPIO.OUT,initial=GPIO.LOW)
-GPIO.setup(BIN1,GPIO.OUT,initial=GPIO.LOW)
-GPIO.setup(AIN2,GPIO.OUT,initial=GPIO.LOW)
-GPIO.setup(BIN2,GPIO.OUT,initial=GPIO.LOW)
-GPIO.setup(conveyor,GPIO.OUT,initial=GPIO.LOW)
+def INIT_2():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(AIN1,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(BIN1,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(AIN2,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(BIN2,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(con,GPIO.OUT,initial=GPIO.LOW)
+
+
+class Position(Enum):
+    TOP = 1
+    MIDDLE = 2
+    BOTTOM = 3
+
+currnet_pos = Position.MIDDLE
+
+def set_default_postion():
+    currnet_pos = Position.MIDDLE
 
 def motor(s,r):
     t = int(s*5/9)
@@ -31,36 +43,13 @@ def motor(s,r):
         
         sig.rotate(r)
 
-global location
-location = 2
-
-# 현재 위치 2, 정상=2, 버림=1, 수리가능=3,4
-def moving(CLASSIFY_RESULT):
-    Way = -1
-    global location
-    
-    if CLASSIFY_RESULT==4:
-        CLASSIFY_RESULT=3
-        
-    move = CLASSIFY_RESULT - location
-    if move<0:
-        move = -move
-        Way = 1
-
-    if move != 0:
-        motor(45*int(move),Way)
-        
-    location = CLASSIFY_RESULT
+def moving( _position:Position ):
+    move_way = currnet_pos - _position
+    motor( 45, move_way )
+    currnet_pos = _position
     
 def run():
-    GPIO.output(conveyor,1)
-    a = 0
-    while sensor.distance_3()==0:
-        time.sleep(0.1)
-        a+=1
-        if a>=25:
-            GPIO.output(conveyor,0)
-            return 1
-    
-    GPIO.output(conveyor,0)
-    return 0
+    GPIO.output(con,1)
+
+def stop():
+    GPIO.output(con,0)
