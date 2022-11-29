9258e4a7d3e264f645bc56c5765800f7d29114d9
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-29 22:12:34 +0900

    Update main.py

--- 모니터 불량 분류/raspberry_pi/main.py
+++ 모니터 불량 분류/raspberry_pi/main.py
@@ -6,22 +6,31 @@ import camera
 import conveyor
 import sensor
 
+global timer_work 
+global start_time 
+global end_time
 timer_work = False
 start_time = time.time()
 end_time = time.time()
 
 def timer_start():
-    timer_check = True
+    global start_time 
+    global timer_work 
+    timer_work = True
     start_time = time.time()
 
 def timer_check():
+    global end_time
+    global start_time 
     end_time = time.time()
     return end_time - start_time
 
 def timer_stop():
+    global timer_work 
     timer_work = False
 
 def timer_working():
+    global timer_work 
     return timer_work
 
 
@@ -33,13 +42,13 @@ def running():
             recv_data = socket_client.recv_d()
             start_sensor_state = sensor.start_pos_detect()
             slide_sensor_state = sensor.slide_detect()
-
-            if recv_data == "START":
+            
+            if True:  # recv_data == "START"
                 if start_sensor_state == 1:
                     capture_img = camera.capture()
                     qr_data = camera.qr(capture_img)
-                    CLASSIFY_RESULT = camera.classify(capture_img)
-                    socket_client.send("RESULT,"+qr_data+","+CLASSIFY_RESULT)
+                    pos = camera.classify(capture_img)
+                    socket_client.send("RESULT,"+str(qr_data)+","+str(pos))
                     
                     pos = conveyor.Position.MIDDLE
                     if pos == 1:
@@ -62,15 +71,19 @@ def running():
             
             if timer_working() and working_time > 2:
                 timer_stop()
+                conveyor.stop()
                 socket_client.send("STUCK")
 
     except Exception as e:
         print( type(e) )
+        print(e)
 
 
 if __name__ == "__main__":
     print( "main" )
-    socket_client.connect_d('192.168.0.35', 8000 )
-    
+    socket_client.connect_d('192.168.0.45', 8000 )
+    sensor.INIT()
+    camera.INIT_1()
+    conveyor.INIT_2()
     main_th = Thread( target=running )
     main_th.start()
