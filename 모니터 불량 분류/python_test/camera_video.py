e2b276aa3547156efd0e11bf932a0748798bd61c
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-30 15:03:32 +0900

    Update and rename mera.py to camera_video.py

--- 모니터 불량 분류/python_test/camera_video.py
+++ 모니터 불량 분류/python_test/camera_video.py
@@ -13,14 +13,3 @@ while True:
 
 capture.release()             
 cv2.destroyAllWindows()
-
-
-# con = 19
-# on = 21
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(con,GPIO.OUT,initial=GPIO.LOW)
-# GPIO.setup(on,GPIO.OUT,initial=GPIO.LOW)
-
-# GPIO.output(con,0)
-# GPIO.output(on,0)
